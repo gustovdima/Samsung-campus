f184d666b248adf8df295f1b871076a2578da3f7
Dmitriy <100062799+gustovdima@users.noreply.github.com>
2022-08-12 13:31:36 +0300

    Update MyRandom.java

--- Fundamentals of Java/Inheritance task 13/Task 2/MyRandom.java
+++ Fundamentals of Java/Inheritance task 13/Task 2/MyRandom.java
@@ -1,1 +1,37 @@
+//Задачи на наследование
 
+/*
+Создать класс MyRandom, наследующий класс Random. 
+В классе MyRandom реализовать метод int nextInt(int from, int to),
+который будет генерировать случайное число в пределах от from до to.
+В это методе не создавать объект класса Random, а использовать super
+*/
+
+import java.util.Random;
+
+public class MyRandom extends Random {
+    private int from;
+    private int to;
+
+    public int nextInt(int from, int to) {
+        int random = from + (int) (Math.random() * to);
+        return random;
+    }
+
+
+    public int getFrom() {
+        return from;
+    }
+
+    public void setFrom(int from) {
+        this.from = from;
+    }
+
+    public int getTo() {
+        return to;
+    }
+
+    public void setTo(int to) {
+        this.to = to;
+    }
+}
