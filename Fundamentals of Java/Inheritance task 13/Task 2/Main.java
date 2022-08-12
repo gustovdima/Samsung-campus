cbd4a81081028e30da983d977c14071871810fd3
Dmitriy <100062799+gustovdima@users.noreply.github.com>
2022-08-12 13:30:36 +0300

    Update Main.java

--- Fundamentals of Java/Inheritance task 13/Task 2/Main.java
+++ Fundamentals of Java/Inheritance task 13/Task 2/Main.java
@@ -7,3 +7,20 @@
 В это методе не создавать объект класса Random, а использовать super
 */
 
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        MyRandom random = new MyRandom();
+        while (true) {
+            System.out.println("Enter num1: ");
+            int num1 = scan.nextInt();
+            System.out.println("Enter num2: ");
+            int num2 = scan.nextInt();
+            System.out.println(random.nextInt(num1, num2));
+        }
+
+    }
+}
