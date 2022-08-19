87376dfe22eb07eeb857c6225d231dcaf6155c63
Dmitriy <100062799+gustovdima@users.noreply.github.com>
2022-08-19 06:00:51 +0300

    Create Films.java

---
+++ Android/Lists in Android/Films.java
@@ -0,0 +1,48 @@
+public class Films {
+  private String title;
+  private int id;
+  private String year; 
+  private String director;
+
+private static int CURRENT_ID = 1;
+
+public Films(String title, String year,String director) {
+    this.director = director;
+    this.title = title;
+    this.id = CURRENT_ID;
+    CURRENT_ID++;
+    this.year = year;
+}
+
+public String getTitle() {
+    return title;
+}
+
+public void setTitle(String title) {
+    this.title = title;
+}
+
+public int getId() {
+    return id;
+}
+
+public void setId(int id) {
+    this.id = id;
+}
+
+public String getYear() {
+    return year;
+}
+
+public void setYear(String year) {
+    this.year = year;
+}
+
+public String getDirector() {
+    return director;
+}
+
+public void setDirector(String director) {
+    this.director = director;
+}
+}
