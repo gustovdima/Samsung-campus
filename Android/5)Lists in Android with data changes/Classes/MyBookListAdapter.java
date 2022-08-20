c136a047c306eda8999708be030805025fa2f77f
Dmitriy <100062799+gustovdima@users.noreply.github.com>
2022-08-20 12:56:08 +0300

    Create MyBookListAdapter.java

---
+++ Android/5)Lists in Android with data changes/Classes/MyBookListAdapter.java
@@ -0,0 +1,40 @@
+package com.example.listtest;
+
+        import android.content.Context;import android.view.LayoutInflater;import android.view.View;import android.view.ViewGroup;import android.widget.ArrayAdapter;import android.widget.TextView;
+
+        import androidx.annotation.Nullable;
+
+        import java.util.ArrayList;
+
+        import Classes.Books;
+
+public class myBookListAdapter extends ArrayAdapter<Books> {
+    Context context;
+    ArrayList<Books> books;
+
+    public myBookListAdapter(Context context, ArrayList<Books> books) {
+        super(context, R.layout.item_books, books);
+        this.context = context;
+        this.books = books;
+    }
+
+    @Override
+    public View getView(int position, @Nullable View convertView, ViewGroup parent) {
+        Books b = this.books.get(position);
+
+        LayoutInflater inflater = LayoutInflater.from(this.context);
+        View v = inflater.inflate(R.layout.item_books, null, false);
+
+        TextView tvTitle = v.findViewById(R.id.tvBookTitle);
+        TextView tvId = v.findViewById(R.id.tvBookId);
+        TextView tvYear = v.findViewById(R.id.tvBookYear);
+        TextView tvAuthor = v.findViewById(R.id.tvBookAuthor);
+
+        tvTitle.setText(b.getTitle());
+        tvYear.setText(b.getYear());
+        tvId.setText(b.getId() + "");
+        tvAuthor.setText(b.getAuthor());
+        v.setTag(b.getId());
+
+        return v;
+    }
