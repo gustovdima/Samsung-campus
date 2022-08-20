5f36abaaa54b15d957244e05fde5d7900ad909a7
Dmitriy <100062799+gustovdima@users.noreply.github.com>
2022-08-20 12:49:30 +0300

    Create Languages.java

---
+++ Android/5)Lists in Android with data changes/Classes/Languages.java
@@ -0,0 +1,50 @@
+import java.io.Serializable;
+
+public class Languages implements Serializable {
+    private String title;
+    private String creator;
+    private String year;
+    private int id;
+
+    private static int CURRENT_ID = 1;
+
+    public Languages(String title, String creator, String year) {
+        this.title = title;
+        this.creator = creator;
+        this.year = year;
+        this.id = CURRENT_ID;
+        CURRENT_ID++;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCreator() {
+        return creator;
+    }
+
+    public void setCreator(String creator) {
+        this.creator = creator;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
