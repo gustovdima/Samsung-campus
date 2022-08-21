2704dabffa14fcbb4aae7ccac07dd189fe5c3ed5
Dmitriy <100062799+gustovdima@users.noreply.github.com>
2022-08-21 13:20:43 +0300

    Delete Android/Lists in Android directory

--- Android/Lists in Android/main_menu.java
+++
@@ -1,6 +0,0 @@
-<?xml version="1.0" encoding="utf-8"?>
-<menu xmlns:android="http://schemas.android.com/apk/res/android"> 
-<item android:id="@+id/itemFilms" android:title="Список фильмов">
-</item> <item android:id="@+id/itemBooks" android:title="Список книг">
-</item> <item android:id="@+id/itemLanguages" android:title="Список языков программирования"></item> 
-</menu>
